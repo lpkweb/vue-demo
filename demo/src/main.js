9a6d035af6867578eff8c8e21e423e913fca6fc0
lipanke <lpkpk_web@163.com>
2020-07-30 16:58:16 +0800

    xxx

--- demo/src/main.js
+++ demo/src/main.js
@@ -2,13 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store';
-import { Calendar, Cell } from 'vant';
+import { Calendar, Cell, ActionSheet } from 'vant';
 import 'vant/lib/index.less';
 
 Vue.config.productionTip = false
 
 Vue.use(Calendar);
 Vue.use(Cell);
+Vue.use(ActionSheet);
 
 new Vue({
   router,
--- demo/src/pages/week-chart/weekChart.vue
+++ demo/src/pages/week-chart/weekChart.vue
@@ -1,12 +1,28 @@
 <template>
   <div>
-    week-chart
+    <van-cell is-link title="基础用法" @click="show = true" />
+    <van-action-sheet v-model="show" :actions="actions" @select="onSelect" />
   </div>
 </template>
 
 <script>
+
+  import { Toast } from 'vant';
+
   export default {
     name: 'weekChart',
+    data() {
+      return {
+        show: false,
+        actions: [{ name: '选项一' }, { name: '选项二' }, { name: '选项三' }],
+      }
+    },
+    methods: {
+      onSelect(item) {
+        this.show = false;
+        Toast(item.name);
+      }
+    }
   }
 </script>
 
